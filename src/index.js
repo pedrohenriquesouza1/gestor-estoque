1163ee8bb8696a5c086adb4e79f88033a09f0eae
pedrohenriquesouza1 <ph3781838@gmail.com>
2026-01-28 02:18:59 -0300

    adicionei novo método e comecei a fazer sistema de busca

--- src/index.js
+++ src/index.js
@@ -11,13 +11,21 @@ class Produto {
     }
 
     calcLucro() {
-        return this.#precoVenda - this. #precoCompra
+        return this.#precoVenda - this.#precoCompra
     }
 
     Resumo() {
-        return this.#nome
+        return `${this.#nome} com ${this.#quant} unidades`
     }
     
+    comparadorNome(nomeBus) {
+        if(this.#nome === nomeBus) {
+            return true
+        } else {
+            return false
+        }
+    }
+    //método acima compara o nome com o nome que vai ser utilizado no search, no caso
 }
 
 const peca1 = new Produto("Óleo 5w40 sintético", 25.90, 39.99, 120);
@@ -25,22 +33,25 @@ console.log(peca1.Resumo());
 console.log(`Lucro por peça: R$ ${peca1.calcLucro().toFixed(2)}`);
 
 class Estoque {
+    #lista
     constructor() {
-        this.lista = []
+        this.#lista = []
     }
 
     adicionar(product) {
-        this.lista.push(product)
-        console.log(`${product.Resumo()} ---- Adicionado com sucesso`)
+        this.#lista.push(product)
+        return `${product.Resumo()}`
+    }
+
+    search() {
+        Produto.comparadorNome("Óleo 5w40 sintético")
+        //continuar a partir daqui, pergunte ao chatGPT ou consulte nas docs como fazer uma busca com boas práticas
     }
 
     remove() {
         //continuar daqui
     }
 
-    search() {
-        //continuar daqui
-    }
 }
 
 const gerenciadorEstoque = new Estoque()
